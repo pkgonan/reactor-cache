a14eebdde2f09007b0b633f676e66f64ba9a2ff3
pkgonan <pkgonan@yanolja.com>
2019-06-06 00:33:20 +0900

    [Fix] Test Case Error

--- reactor-cache-spring/src/test/java/reactor/cache/spring/SpringFluxCacheTest.java
+++ reactor-cache-spring/src/test/java/reactor/cache/spring/SpringFluxCacheTest.java
@@ -1,6 +1,5 @@
 package reactor.cache.spring;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.cache.Cache;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
@@ -10,19 +9,12 @@ import reactor.core.publisher.Flux;
 
 public class SpringFluxCacheTest {
 
-    private SpringFluxCache<String> springFluxCache;
-
-    @Before
-    public void init() {
+    @Test(expected = NotSupportException.class)
+    public void find() {
         final String cacheName = "Flux-cache-test";
         final Cache cache = new ConcurrentMapCache(cacheName);
         final Class<String> cacheRegionType = String.class;
 
-        this.springFluxCache = new SpringFluxCache<>(cache, cacheRegionType);
-    }
-
-    @Test(expected = NotSupportException.class)
-    public void find() {
-        springFluxCache.find(Flux.empty(), "");
+        final SpringFluxCache<String> springFluxCache = new SpringFluxCache<>(cache, cacheRegionType);
     }
 }
--- reactor-cache-spring/src/test/java/reactor/cache/spring/SpringMonoCacheTest.java
+++ reactor-cache-spring/src/test/java/reactor/cache/spring/SpringMonoCacheTest.java
@@ -6,6 +6,9 @@ import org.springframework.cache.Cache;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
 import reactor.cache.spring.core.SpringMonoCache;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
 
 import static org.junit.Assert.assertEquals;
 
@@ -30,7 +33,12 @@ public class SpringMonoCacheTest {
         final Mono<String> retriever = Mono.just("Value");
         final String key = "Key";
 
-        springMonoCache.find(retriever, key).subscribe(result::append);
+        final Mono<String> stringMono = springMonoCache.find(retriever, key).doOnSuccess(result::append);
+
+        StepVerifier.create(stringMono)
+                .expectNext("Value")
+                .expectComplete()
+                .verify();
 
         assertEquals(expected.toString(), result.toString());
     }
